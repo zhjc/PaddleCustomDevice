eb66a5e38f199c5c4a784a09efb1380b4a41a0f3
max-niu <57517234+max-niu@users.noreply.github.com>
2023-07-13 20:44:26 +0800

    Update ffn_op.cc

--- backends/npu/custom_op/ffn_op.cc
+++ backends/npu/custom_op/ffn_op.cc
@@ -15,24 +15,93 @@
 #include <iostream>
 #include <vector>
 
+#include "kernels/funcs/npu_op_runner.h"
 #include "paddle/extension.h"
 
-std::vector<paddle::Tensor> MyFfnOp(const paddle::Tensor& x,
-                                     const paddle::Tensor& y) {
-  // ONLY FOR PASS TEST
-  return {paddle::add(x, y)};
+std::vector<std::vector<int64_t>> FFNOpInferShape(
+    const std::vector<int64_t>& input_shape,
+    const std::vector<int64_t>& weight_shape,
+    const std::vector<int64_t>& bias_shape) {
+  std::vector<int64_t> out_shape = input_shape;
+  out_shape[out_shape.size() - 1] = weight_shape[weight_shape.size() - 1];
+  return {out_shape};
 }
 
-std::vector<std::vector<int64_t>> MyFfnOpInferShape(
-    const std::vector<int64_t>& x_shape,
-    const std::vector<int64_t>& y_shape) {
-  // ONLY FOR PASS TEST        
-  return {y_shape};
+std::vector<paddle::Tensor> FFNOp(
+    const paddle::Tensor& input,
+    const paddle::Tensor& weight,
+    const paddle::Tensor& bias) {
+  // note: only for case of [B, M, K] x [K, N] = [B, M, N].
+  auto dev_ctx = static_cast<const phi::CustomContext*>(
+      paddle::experimental::DeviceContextPool::Instance().Get(input.place()));
+  auto stream = static_cast<aclrtStream>(dev_ctx->stream());
+  
+  auto x = static_cast<const phi::DenseTensor*>(input.impl().get());
+  auto y = static_cast<const phi::DenseTensor*>(weight.impl().get());
+  auto b = static_cast<const phi::DenseTensor*>(bias.impl().get());
+  
+  std::vector<int64_t> x_dims = phi::vectorize(x->dims());
+  std::vector<int64_t> y_dims = phi::vectorize(y->dims());
+  int x_ndim = x_dims.size();
+  int y_ndim = y_dims.size();
+  
+  // x->x_temp : [B, M, K]->[B*M, K]
+  phi::DenseTensor x_temp(*x);
+  const int K = x_dims[x_ndim - 1];
+  std::vector<int64_t> vec_dim = {x_temp.numel() / K, K};
+  x_temp.Resize(phi::make_ddim(vec_dim));
+
+  // out
+  std::vector<int64_t> temp_dims = {x_temp.numel() / K, y_dims[y_ndim - 1]};
+  
+  std::shared_ptr<phi::DenseTensor> matmul_res =
+      std::make_shared<phi::DenseTensor>();
+  matmul_res->Resize(phi::make_ddim(temp_dims));
+  dev_ctx->Alloc(matmul_res.get(), x->dtype());
+  
+  std::shared_ptr<phi::DenseTensor> add_res =
+      std::make_shared<phi::DenseTensor>();
+  add_res->Resize(phi::make_ddim(temp_dims));
+  dev_ctx->Alloc(add_res.get(), x->dtype());
+  
+  std::shared_ptr<phi::DenseTensor> out =
+      std::make_shared<phi::DenseTensor>();
+  out->Resize(phi::make_ddim(temp_dims));
+  dev_ctx->Alloc(out.get(), x->dtype());
+  
+  // attr
+  const bool transpose_x1 = false;
+  const bool transpose_x2 = false;
+  NPUAttributeMap attrs = {{"transpose_x1", transpose_x1}, {"transpose_x2", transpose_x2}};
+ 
+  // run
+  const auto& runner1 =
+      NpuOpRunner("MatMul", {x_temp, *y}, {*matmul_res}, attrs);
+  runner1.Run(stream);
+  
+  const auto& runner2 =
+      NpuOpRunner("Add", {*matmul_res, *b}, {*add_res});
+  runner2.Run(stream);
+  
+  const auto& runner3 =
+      NpuOpRunner("Gelu", {*add_res}, {*out}, {});
+  runner3.Run(stream);
+  
+  
+  // post
+  auto out_dims = FFNOpInferShape(
+      input.shape(), weight.shape(), bias.shape()).at(0);
+  out->Resize(phi::make_ddim(out_dims));  
+  auto out_tensor = paddle::Tensor(out);
+
+  std::cout<<"ffn1"<<std::endl;
+  return {out_tensor};
+  // return {paddle::gpaddle::add(paddle::matmul(input, weight), bias)};
 }
 
-PD_BUILD_OP(Ffn_pass_test)
-    .Inputs({"X", "Y"})
+PD_BUILD_OP(ffn)
+    .Inputs({"Input", "Weight", "Bias"})
     .Outputs({"Out"})
-    .SetKernelFn(PD_KERNEL(MyFfnOp))
+    .SetKernelFn(PD_KERNEL(FFNOp))
     .SetInferShapeFn(PD_INFER_SHAPE(
-        MyFfnOpInferShape));  // neccessary if the op has muti_inputs
+        FFNOpInferShape));  // neccessary if the op has muti_inputs
