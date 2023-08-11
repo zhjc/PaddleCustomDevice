9272e4275afe59ec4b7df47d1a3dd10adc089d37
hejingkui <hejingkui1@huawei.com>
2023-08-11 16:03:35 +0800

    add double assign pass

---
+++ backends/npu/passes/double_assign_pass.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, division
+
+import os
+import numpy as np
+import unittest
+import paddle
+
+@paddle.incubate.passes.ir.RegisterPass
+def generate_ffn():
+    def pattern(x, y):
+        return paddle.assign(x), paddle.assign(y)
+
+    def replace(x, y, bias):
+        return x, y
+
+    return pattern, replace
